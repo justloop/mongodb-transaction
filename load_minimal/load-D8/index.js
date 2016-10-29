5b5fb602a34684146884b218e7b5c9365f1b722b
gejun <yuenengfanhua@gmail.com>
2016-10-29 21:57:30 +0800

    remove sharding logic

--- load_minimal/load-D8/index.js
+++ load_minimal/load-D8/index.js
@@ -7,9 +7,3 @@ db.order2.createIndex({o_id:-1 ,o_w_id:1, o_d_id:1});
 db.order2.createIndex({o_carrier_id: 1});
 db.item.createIndex({i_id: 1, i_w_id: 1});
 
-sh.enableSharding("D8");
-sh.shardCollection("D8.warehouse", { _id : 1 } )
-sh.shardCollection("D8.district", { _id : 1 } )
-sh.shardCollection("D8.customer", { _id : 1 } )
-sh.shardCollection("D8.order2", { _id : 1 } )
-sh.shardCollection("D8.item", { _id : 1 } )
