f6474834d3ef2fafd27daaff445f4a07f31089e5
gejun <yuenengfanhua@gmail.com>
2016-11-01 10:34:20 +0800

    fix

--- benchmark_runner.sh
+++ benchmark_runner.sh
@@ -5,7 +5,7 @@ for data in "D8" "D40"
 do
 	for clients in 10 20 40
 	do
-		echo "cd ../benchmark ; python benchmark.py ${data} ${clients} > benchmark-${data}-${clients}.log"
-		cd ../benchmark ; python benchmark.py ${data} ${clients} 2>&1 > benchmark-${data}-${clients}.log
+		echo "cd benchmark ; python benchmark.py ${data} ${clients} > benchmark-${data}-${clients}.log"
+		cd benchmark ; python benchmark.py ${data} ${clients} 2>&1 > benchmark-${data}-${clients}.log
 	done
 done
