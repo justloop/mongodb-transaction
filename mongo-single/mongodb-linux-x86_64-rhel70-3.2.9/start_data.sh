c283bdf9162a241f8f8e1f9e9fffde5df3242203
gejun <yuenengfanhua@gmail.com>
2016-11-06 16:24:44 +0800

    add single mongo instance set up and 3shard results

---
+++ mongo-single/mongodb-linux-x86_64-rhel70-3.2.9/start_data.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+/temp/mongo-single/mongodb-linux-x86_64-rhel70-3.2.9/bin/mongod --config /temp/mongo-single/mongodb-linux-x86_64-rhel70-3.2.9/set10.conf
+echo "MongoDB set10 start successfully..."
+
--- mongodb-linux-x86_64-rhel70-3.2.9/stop.sh
+++ mongodb-linux-x86_64-rhel70-3.2.9/stop.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-kill -2 `cat /temp/mongodb-linux-x86_64-rhel70-3.2.9/configsvr.pid`
-echo "MongoDB config server stop successfully"
-kill -2 `cat /temp/mongodb-linux-x86_64-rhel70-3.2.9/mongos.pid`
-echo "MongoDB mongos server stop successfully"
-kill -2 `cat /temp/mongodb-linux-x86_64-rhel70-3.2.9/set10.pid`
-echo "MongoDB set10 stop successfully"
-
+ssh xcnd9 "pkill -f mongo"
+ssh xcnd10 "pkill -f mongo"
+ssh xcnd11 "pkill -f mongo"
+echo "MongoDB stop successfully"
